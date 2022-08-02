e8d8eedc14c3033f70be6b5dba1ca52474f69f6a
Nguyen Thi Dieu <thidieu2003@gmail.com>
2022-08-02 16:34:10 +0700

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+	@GetMapping("/")
+	public String home(){
+		return "hello world!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
