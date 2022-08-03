4e3dd63be1d9344ee630a3008f2a12c8fe35baf0
Nguyen Thi Dieu <thidieu2003@gmail.com>
2022-08-03 22:27:27 +0700

    aa

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ import javax.annotation.Resource;
 public class DemoApplication {
 	@GetMapping("/home")
 	public String home(){
-		return "Chuc mung ban da deploy thanh cong nha aaaaaaaa!!";
+		return "Chuc mung ban da deploy thanh cong nha nnnnn!!";
 	}
 
 	public static void main(String[] args) {
